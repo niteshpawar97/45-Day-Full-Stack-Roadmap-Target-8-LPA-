f7f281b32559c0045f0651d6b57dec8605b5c085
niteshpawar97 <niteshpawar97@gmail.com>
2025-12-05 10:09:24 +0530

    done

---
+++ Day_01/basic.js
@@ -0,0 +1,7 @@
+console.log("Hello, World!");
+console.warn("This is a warning message.");
+console.error("This is an error message.");
+console.info("This is an informational message.");
+console.debug("This is a debug message.");
+console.table([{name: "Alice", age: 30}, {name: "Bob", age: 25}]);
+console.time("Timer");
